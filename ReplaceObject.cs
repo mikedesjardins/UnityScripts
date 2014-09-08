e8542b80f2ce8727aaf737330e27055afc54b819
Mike Desjardins <mike.desjardins@outlook.com>
2014-09-07 23:00:30 -0400

    commit

---
+++ ReplaceObject.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public class ReplaceObject : EditorWindow {
+
+	GameObject[] objs;
+	GameObject objReplacement;
+	bool _canReplace;
+	
+	[MenuItem ("Replace Object/Replace")]
+	static void Init () {
+		ReplaceObject window = (ReplaceObject)EditorWindow.GetWindow (typeof (ReplaceObject));
+	}
+	
+	void OnGUI () {
+		objReplacement = (GameObject)EditorGUILayout.ObjectField("Replacement",objReplacement, typeof(GameObject),true);
+		if(GUILayout.Button("Replace Object")){
+			if(objs.Length != 0 && objReplacement){
+				_canReplace = true;
+			}
+		}
+	}
+	
+	void Update(){
+		objs = Selection.gameObjects;
+		if(_canReplace){
+			Replace();
+		}
+	}
+	
+	void Replace(){
+		foreach(GameObject obj in objs){
+			GameObject newObj = PrefabUtility.InstantiatePrefab(objReplacement) as GameObject;
+			newObj.transform.position = obj.transform.position;
+			newObj.transform.parent = obj.transform.parent;
+			DestroyImmediate(obj);
+		}
+		_canReplace = false;
+	}
+}
